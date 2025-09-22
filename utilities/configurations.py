b640f72e0bcce1c263e3c63b0f843fcf01c41dea
Abdul-ur-Rehman <chattha926242@gmail.com>
2025-09-22 13:50:05 +0500

    hiding token

--- utilities/configurations.py
+++ utilities/configurations.py
@@ -6,4 +6,6 @@ def getconfig():
     return config
 
 def getPassword():
-    return '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'
+    with open(r"C:\Users\sg\Downloads\secret_token.txt", "r") as token:
+        github_token = token.read().strip()
+        return github_token

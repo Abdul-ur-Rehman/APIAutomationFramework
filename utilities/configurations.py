a56f0940df67c233d10b5903b81b84f20c6c0176
Abdul-ur-Rehman <chattha926242@gmail.com>
2025-09-24 15:00:06 +0500

    Configurations for database connection

--- utilities/configurations.py
+++ utilities/configurations.py
@@ -9,18 +9,19 @@ def getconfig():
     config.read("utilities/properties.ini")
     return config
 
+
+def getPassword():
+    with open(r"C:\Users\sg\Downloads\secret_token.txt", "r") as token:
+        github_token = token.read().strip()
+        return github_token
+
+# Configurations for database connection
 connect_config = {
     "host" : getconfig()['SQL']['host'],
     "database" : getconfig()['SQL']['database'],
     "user" : getconfig()['SQL']['user'],
     "password" : getconfig()['SQL']['password']
 }
-
-
-def getPassword():
-    with open(r"C:\Users\sg\Downloads\secret_token.txt", "r") as token:
-        github_token = token.read().strip()
-        return github_token
 
 def getConnection():
     try:
